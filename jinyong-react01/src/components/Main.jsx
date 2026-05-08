1ea0de0cbb270134afdafefdc49e3b5696654774
hjn5018 <hjn5018@gmail.com>
2026-05-08 09:11:53 +0900

    chore3

--- jinyong-react01/src/components/Main.jsx
+++ jinyong-react01/src/components/Main.jsx
@@ -9,15 +9,24 @@ let Main = () => {
     return (
         <>
         <h1>메인 영역</h1>
+
         저는 {var1} 대학에 다니고, {var2 - 5}년 후에는 {var3}, {var4} <br />
+
         {var3 === true ? "로그인" : "로그아웃"} <br />
+
         {String(var3)} <br />
+
         {"10" + 5} <br />
+
         {Number("10") + 5} <br />
+
         {String(Boolean("10"))} <br />
+
         {String(undefined)} <br />
+
         <br />
         <br />
+        
         {/* {var5}  */}
         {var5.name}
         
