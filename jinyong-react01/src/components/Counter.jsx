bee0913d230582567baf15e740c6f3bf7d97d64c
hjn5018 <hjn5018@gmail.com>
2026-05-08 09:02:47 +0900

    counter

--- jinyong-react01/src/components/Counter.jsx
+++ jinyong-react01/src/components/Counter.jsx
@@ -5,8 +5,10 @@ export default function Counter() {
     let [count, setCount] = useState(0);
 
     var btnClick = () => {
-        setCount(++count);
-        // setCount(count++);
+        setCount(count => count + 1); // O
+        // setCount(count + 1); // O
+        // setCount(++count); // O
+        // setCount(count++); //X
     }
 
     return (
