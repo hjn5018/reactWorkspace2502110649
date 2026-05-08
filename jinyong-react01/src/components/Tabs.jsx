b7d8cf821fb139fef6176e1d25c4e25e58383f3e
hjn5018 <hjn5018@gmail.com>
2026-05-08 09:03:10 +0900

    렌더가 가능한 것들과 아닌 것들

--- jinyong-react01/src/components/Tabs.jsx
+++ jinyong-react01/src/components/Tabs.jsx
@@ -10,6 +10,10 @@ export default function Tabs() {
             <button onClick={()=>{setActiveTab("about")}}>Aome</button>
             { activeTab === "home" && <p>Welcome Home!</p> }
             { activeTab === "about" && <p>About Us!</p> }
+
+            {/* false, null, undefined, true는 렌저링하지 않는다. 하지만 -, '', NaN은 falsy임에도 렌더링된다. */}
+            {false}
+            {0}
         </>
     );
 }
